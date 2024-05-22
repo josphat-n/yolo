344893091f38d0580db724da6269f731a4637bfa
josphat-n <josphatnjoroge254@gmail.com>
2024-05-22 16:16:32 +0300

    Remove forward slash to allow proper proxying with nginx.

--- client/src/components/ProductControl.js
+++ client/src/components/ProductControl.js
@@ -78,7 +78,7 @@ class ProductControl extends Component {
     }
     
     componentDidMount(){
-        axios.get('/api/products')
+        axios.get('api/products')
             .then(res =>{
                 console.log(res)
                 this.setState({
@@ -157,14 +157,14 @@ class ProductControl extends Component {
         //     console.log(pair[0]+ ', ' + pair[1]); 
         // }       
         // console.log(...formData)
-        axios.post('/api/products', newProduct)
+        axios.post('api/products', newProduct)
             .then(res => console.log(res.data))
         this.setState({
             formVisibleOnPage: false
         })
     };
     handleDeletingProduct = (id) =>{
-        axios.delete('/api/products/'+id)
+        axios.delete('api/products/'+id)
             .then(res => console.log(res.data))
             .catch((error) =>{
                 console.log(error)
@@ -184,7 +184,7 @@ class ProductControl extends Component {
     }
     handleEditingProduct = (editedProduct) =>{
 
-        axios.put('/api/products/' + this.state.selectedProduct._id, editedProduct)
+        axios.put('api/products/' + this.state.selectedProduct._id, editedProduct)
             .then(res =>console.log(res.data))
         
         this.setState({
